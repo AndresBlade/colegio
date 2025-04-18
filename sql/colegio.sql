569724a237020c989f52d10c5b7e84743c562966
Andres Arrieche <andresarrieche11@gmail.com>
2025-04-18 09:45:38 -0400

    Modificada tabla de prestamos para que incluya empleado y cargo

--- sql/colegio.sql
+++ sql/colegio.sql
@@ -421,11 +421,15 @@ DROP TABLE IF EXISTS `peticion_prestamo`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `peticion_prestamo` (
   `id` int NOT NULL AUTO_INCREMENT,
+  `empleado_id` int NOT NULL,
+  `cargo_id` int NOT NULL,
   `tipo_remuneracion_id` int NOT NULL,
   `aceptada` tinyint NOT NULL,
   `monto` decimal(10,2) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_peticion_prestamo_tipo_remuneracion_idx` (`tipo_remuneracion_id`),
+  KEY `fk_peticion_prestamo_empleado_idx` (`empleado_id`,`cargo_id`),
+  CONSTRAINT `fk_peticion_prestamo_empleado` FOREIGN KEY (`empleado_id`, `cargo_id`) REFERENCES `empleado_cargo` (`empleado_id`, `cargo_id`),
   CONSTRAINT `fk_peticion_prestamo_tipo_remuneracion` FOREIGN KEY (`tipo_remuneracion_id`) REFERENCES `tipo_remuneracion` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -630,4 +634,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-04-17  9:41:15
+-- Dump completed on 2025-04-18  9:43:39
